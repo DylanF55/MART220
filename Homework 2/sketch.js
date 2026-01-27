8e922681706381110761946322c7eae1975ba3ca
DylanF55 <dylan.fullerton@umconnect.umt.edu>
2026-01-26 21:00:39 -0700

    fn

--- Homework 2/sketch.js
+++ Homework 2/sketch.js
@@ -1,8 +1,20 @@
   function setup()
             {
-                createCanvas(300,300);
+                createCanvas(900,900);
             }
             function draw(){
+                background(0);
+                fill(0,100,255);
                 circle(150,150,50);
-                background(210);
+                circle(150,210,50);
+                circle(150,270,50);
+                circle(450,150,50);
+                circle(450,210,50);
+                circle(450,270,50);
+
+                fill(0, 89, 28);
+                ellipse(600, 50, 80, 400);
+                ellipse(50, 50, 80, 400);
+                fill(0,100,255);
+                ellipse(300, 500, 10, 200);
             }
