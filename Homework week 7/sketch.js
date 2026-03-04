da1bee90fdceb5ecec9e1abdc39ad29a2fc18b8d
DylanF55 <dylan.fullerton@umconnect.umt.edu>
2026-03-04 05:23:07 -0700

    t

--- Homework week 7/sketch.js
+++ Homework week 7/sketch.js
@@ -5,6 +5,10 @@ let imgY = 10;
 let imgX2 = 400;
 let imgY2 = 400;
 
+// timer//
+let startTime;
+let TOTAL_SECONDS = 60;
+
 function preload() {
     img = loadImage('images/move1.png');
     img2 = loadImage('images/food1.jpg');
@@ -12,11 +16,24 @@ function preload() {
 
 function setup() {
     createCanvas(1500, 1000);
+    startTime = millis(); 
 }
 
 function draw() {
-    background(120);imgX2 += random(-2, 2);
-    imgY2 += random(-2, 2);
+    background(120);
+    
+    imgX2 += random(-100, 100);
+    imgY2 += random(-100, 100);
+
+    let elapsed = (millis() - startTime) / 1000;                  
+    let remaining = max(0, TOTAL_SECONDS - elapsed);            
+    let secs = floor(remaining);
+
+
+    fill(255);
+    textSize(32);
+    textAlign(LEFT, TOP);
+    text(nf(secs, 2), 10, 10);
 
     // Controls//
     if (keyIsDown(LEFT_ARROW)) imgX -= 5;
