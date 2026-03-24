3f1af88412ae44db16963a894595237378fc25bb
DylanF55 <dylanfullerton83@gmail.com>
2026-03-23 23:52:35 -0600

    c

--- Homework week 9/sketch.js
+++ Homework week 9/sketch.js
@@ -8,11 +8,14 @@ let score = 0;
 let isNearFood = false;
 let song;
 let audioStarted = false;
-let playerCollider;
-let blockBox;
-
-const PLAYER_W = 400;
-const PLAYER_H = 300;
+let playerW = 400;
+let playerH = 300;
+let blockSize = 140;
+let SphereX = 180;
+let SphereY = 860;
+let SphereRadius = 20;
+let SphereDiameter = SphereRadius * 2;
+let sphereVisible = true;
 
 // timer//
 let startTime;
@@ -30,17 +33,20 @@ function setup() {
     startTime = millis();
     song.setVolume(0.1);
 
-    // Invisible sprite that follows move1.png and handles collision physics.
-    playerCollider = createSprite(imgX + PLAYER_W / 2, imgY + PLAYER_H / 2, 140, 110);
-    playerCollider.visible = false;
-
-    // Visible p5.play box obstacle.
-    blockBox = createSprite(700, 420, 220, 220);
-    blockBox.shapeColor = color(220, 70, 70);
+    // player boundary//
+    imgX = width / 2 - playerW / 2;
+    imgY = height / 2 - playerH / 2;
 }
 
 function draw() {
     background(120);
+
+    let cornerBlocks = [
+
+        { x: width - blockSize, y: 0, w: blockSize, h: blockSize },
+        { x: 0, y: height - blockSize, w: blockSize, h: blockSize },
+        { x: width - blockSize, y: height - blockSize, w: blockSize, h: blockSize }
+    ];
     
     imgX2 += random(-100, 100);
     imgY2 += random(-100, 100);
@@ -61,20 +67,39 @@ function draw() {
         text('click to begin', 400, 400);
     }
 
-    // Controls with collision against the p5.play box.
+    // Controls and collision for corner blocks.
     let nextX = imgX;
     let nextY = imgY;
+
     if (keyIsDown(LEFT_ARROW)) nextX -= 5;
     if (keyIsDown(RIGHT_ARROW)) nextX += 5;
     if (keyIsDown(UP_ARROW)) nextY -= 5;
     if (keyIsDown(DOWN_ARROW)) nextY += 5;
 
-    playerCollider.position.x = nextX + PLAYER_W / 2;
-    playerCollider.position.y = nextY + PLAYER_H / 2;
-    playerCollider.collide(blockBox);
+    nextX = constrain(nextX, 0, width - playerW);
+    nextY = constrain(nextY, 0, height - playerH);
 
-    imgX = playerCollider.position.x - PLAYER_W / 2;
-    imgY = playerCollider.position.y - PLAYER_H / 2;
+    let blockedX = false;
+    for (let block of cornerBlocks) {
+        if (rectsOverlap(nextX, imgY, playerW, playerH, block.x, block.y, block.w, block.h)) {
+            blockedX = true;
+            break;
+        }
+    }
+    if (!blockedX) {
+        imgX = nextX;
+    }
+
+    let blockedY = false;
+    for (let block of cornerBlocks) {
+        if (rectsOverlap(imgX, nextY, playerW, playerH, block.x, block.y, block.w, block.h)) {
+            blockedY = true;
+            break;
+        }
+    }
+    if (!blockedY) {
+        imgY = nextY;
+    }
         //Scoring for proximity to food//
     let playerCenterX = imgX + 90;
     let playerCenterY = imgY + 150;
@@ -92,10 +117,35 @@ function draw() {
         isNearFood = false;
     }
 
-    
-    if (img) image(img, imgX, imgY, 400, 300);
+    if (sphereVisible && circleRectOverlap(SphereX, SphereY, SphereRadius, imgX, imgY, playerW, playerH)) {
+        score = max(0, score - 1);
+        sphereVisible = false;
+    }
+
+    fill(0, 100, 255);
+    noStroke();
+    for (let block of cornerBlocks) {
+        rect(block.x, block.y, block.w, block.h);
+    }
+
+    if (sphereVisible) {
+        fill(180, 120, 255);
+        circle(SphereX, SphereY, SphereDiameter);
+    }
+
+    if (img) image(img, imgX, imgY, playerW, playerH);
     if (img2) image(img2, imgX2, imgY2, 200, 150);
-    drawSprites();
+}
+
+function rectsOverlap(ax, ay, aw, ah, bx, by, bw, bh) {
+    return ax < bx + bw && ax + aw > bx && ay < by + bh && ay + ah > by;
+}
+
+function circleRectOverlap(cx, cy, cr, rx, ry, rw, rh) {
+    let nearestX = constrain(cx, rx, rx + rw);
+    let nearestY = constrain(cy, ry, ry + rh);
+    let d = dist(cx, cy, nearestX, nearestY);
+    return d <= cr;
 }
 
 function mousePressed() {
